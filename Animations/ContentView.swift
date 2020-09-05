07c03e651082f0b1f11694330195e99a9b00c89b
Rafael Calunga <rafaelcalunga@gmail.com>
2020-09-05 16:34:06 -0400

    Initial Commit

---
+++ Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Rafael Calunga on 2020-09-05.
+//  Copyright © 2020 Rafael Calunga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
